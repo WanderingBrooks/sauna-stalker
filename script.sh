701a37e506ced76372e718db42fc9ec8418dccb2
Jason Brooks <cs.jasonbrooks@gmail.com>
2024-12-15 12:26:41 +0100

    Update scripts

--- script.sh
+++ script.sh
@@ -1,3 +1,11 @@
 #!/bin/bash
 
-$(which node) ./dist/index.js
+SCRIPT_DIR="$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)"
+
+echo "$SCRIPT_DIR"
+
+set -a  # Automatically export variables
+source "$SCRIPT_DIR/.env"
+set +a  # Disable automatic export
+
+$(which node) $SCRIPT_DIR/dist/index.js;
